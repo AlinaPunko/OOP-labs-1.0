e9b7ae510f4d02e972dc8fe0856254ddcb949146
AlinaPunko <punko.alina2000@gmail.com>
2019-08-27 23:28:55 +0300

    C# labs 1.0

---
+++ Lab12/Lab12/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+
+    interface Interable
+    {
+        void toString();
+    }
+
+    public abstract class Transport : Interable
+    {
+        protected int number;
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+        public int speed;
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                speed = value;
+            }
+        }
+        protected int cost;
+        public int Cost
+        {
+            get
+            {
+                return cost;
+            }
+            set
+            {
+                cost = value;
+            }
+        }
+        virtual public void toString()
+        {
+            Console.WriteLine("Используем toString в классе Transport Номер " + number + " Скорость " + speed + " Объект едет в " + cost);
+        }
+
+        internal Transport(int a, int b, int c)
+        {
+            number = 0;
+            cost = 0;
+            speed = 0;
+        }
+        internal Transport()
+        {
+            number = 0;
+            cost = 0;
+            speed = 0;
+        }
+    }
+    public class Reflector
+    {
+        public int Sq(int a)
+        { return a ^ 2; }
+        public Type type;
+        public Reflector(string type)
+        {
+            this.type = Type.GetType(type, false, true);
+        }
+        public void AboutClass()
+        {
+            using (FileStream fstream = new FileStream("class.txt", FileMode.OpenOrCreate))
+            {
+                foreach (MemberInfo info in type.GetMembers())
+                {
+                    byte[] array = Encoding.Default.GetBytes(info.DeclaringType + " - " + info.MemberType + " - " + info.Name + " \n");
+                    fstream.Write(array, 0, array.Length);
+                }
+            }
+        }
+        public void PublicMethods()
+        {
+            using (FileStream fstream = new FileStream("methods.txt", FileMode.OpenOrCreate))
+            {
+                foreach (MethodInfo method in type.GetMethods())
+                {
+                    if (method.IsPublic)
+                    {
+                        byte[] array = Encoding.Default.GetBytes(method.Name + " \n");
+                        fstream.Write(array, 0, array.Length);
+                    }
+                }
+            }
+        }
+        public void SpecifiedMethods(string arg)
+        {
+            using (FileStream fstream = new FileStream("specified_methods.txt", FileMode.OpenOrCreate))
+            {
+                foreach (MethodInfo method in type.GetMethods())
+                {
+                    ParameterInfo[] parameters = method.GetParameters();
+                    for (int i = 0; i < parameters.Length; i++)
+                    {
+                        if (arg.Contains(parameters[i].ParameterType.Name))
+                        {
+                            byte[] array1 = Encoding.Default.GetBytes(method.ReturnType.Name + " - " + method.Name + " ( ");
+                            fstream.Write(array1, 0, array1.Length);
+                            byte[] array2 = Encoding.Default.GetBytes(parameters[i].ParameterType.Name + " " + parameters[i].Name);
+                            fstream.Write(array2, 0, array2.Length);
+                            byte[] array3 = Encoding.Default.GetBytes(" , ");
+                            if (i + 1 < parameters.Length)
+                            {
+                                fstream.Write(array3, 0, array3.Length);
+                            }
+                            fstream.Write(Encoding.Default.GetBytes(") \n"), 0, Encoding.Default.GetBytes(") \n").Length);
+                        }
+                    }
+                }
+            }
+        }
+        public void Fields()
+        {
+            using (FileStream fstream = new FileStream("fields.txt", FileMode.OpenOrCreate))
+            {
+                foreach (FieldInfo field in type.GetFields())
+                {
+                    byte[] array = Encoding.Default.GetBytes(field.FieldType + " - " + field.Name + "\n");
+                    fstream.Write(array, 0, array.Length);
+                }
+            }
+        }
+        public void Properties()
+        {
+            using (FileStream fstream = new FileStream("properties.txt", FileMode.OpenOrCreate))
+            {
+                foreach (PropertyInfo prorertie in type.GetProperties())
+                {
+                    byte[] array = Encoding.Default.GetBytes(prorertie.PropertyType + " - " + prorertie.Name + " \n");
+                    fstream.Write(array, 0, array.Length);
+                }
+            }
+        }
+        public void Interfaces()
+        {
+            using (FileStream fstream = new FileStream("interfaces.txt", FileMode.OpenOrCreate))
+            {
+                foreach (Type interfaces in type.GetInterfaces())
+                {
+                    byte[] array = Encoding.Default.GetBytes(interfaces.Name + "\n");
+                    fstream.Write(array, 0, array.Length);
+                }
+            }
+        }
+        public void RunTimeMethod(string type, string method)
+        {
+            Assembly asm = Assembly.LoadFrom(@"E:\лабы\ООП\Lab12\bin\Debug\Lab12.exe");
+            Type newType = asm.GetType(type);
+            int k = 3;
+            object programm = Activator.CreateInstance(newType, k);
+            MethodInfo newMethod = newType.GetMethod(method);
+            object result = newMethod.Invoke(programm, new object[] { });
+            Console.WriteLine("Method of another instanse: {0}\nMethod's value: {1}", method, result);
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Reflector reflector = new Reflector("Lab12.Transport");
+            reflector.AboutClass();
+            reflector.PublicMethods();
+            reflector.Fields();
+            reflector.Properties();
+            reflector.Interfaces();
+            reflector.SpecifiedMethods("Object");
+        }
+    }
+}
+
---
+++ Lab13/Lab13/Program.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.IO.Compression;
+namespace Lab13
+{
+    static class PAADirInfo
+    {
+        public static void FileCount(string path)
+        {
+            Console.WriteLine("Количество файлов в каталоге {0}: {1}", path, Directory.GetFiles(path).Length);
+            Console.WriteLine();
+        }
+        public static void CreationTime(string path)
+        {
+            Console.WriteLine("Время создания каталога {0}: {1}", path, Directory.GetCreationTime(path));
+            Console.WriteLine();
+        }
+        public static void SubdirectoriesCount(string path)
+        {
+            Console.WriteLine("Количество подкаталогов в каталоге {0}: {1}", path, Directory.GetDirectories(path).Length);
+            Console.WriteLine();
+        }
+        public static void ParentDirectory(string path)
+        {
+            Console.WriteLine("Родительский каталог каталога {0}: {1}", path, Directory.GetParent(path));
+            Console.WriteLine();
+        }
+
+    }
+    static class PAADiskInfo
+    {
+        public static void FreeSpace(string driveName)
+        {
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.Name == driveName && drive.IsReady)
+                    Console.WriteLine("Доступный объем на диске {0} : {1}", driveName.First(), drive.AvailableFreeSpace);
+            }
+            Console.WriteLine();
+        }
+        public static void FileSystemInfo(string driveName)
+        {
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.Name == driveName && drive.IsReady)
+                    Console.WriteLine("Тип файловой системы и формат диска {0} : {1}, {2}", driveName.First(), drive.DriveType, drive.DriveFormat);
+            }
+            Console.WriteLine();
+        }
+        public static void DrivesFullInfo()
+        {
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    Console.WriteLine("Имя: {0}", drive.Name);
+                    Console.WriteLine("Объем: {0}", drive.TotalSize);
+                    Console.WriteLine("Доступный объем: {0}", drive.AvailableFreeSpace);
+                    Console.WriteLine("Метка тома: {0}", drive.VolumeLabel);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+    static class PAAFileInfo
+    {
+        public static void FullPath(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            Console.WriteLine("Полный путь к файлу {0}: {1}", fileInfo.Name, fileInfo.FullName);
+            Console.WriteLine();
+        }
+        public static void BasicFileInfo(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            Console.WriteLine("Имя файла: {0}", fileInfo.Name);
+            Console.WriteLine("Расширение файла: {0}", fileInfo.Extension);
+            Console.WriteLine("Размер файла: {0}", fileInfo.Length);
+            Console.WriteLine();
+        }
+        public static void CreationTime(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            Console.WriteLine("Время создания файла {0}: {1}", fileInfo.Name, fileInfo.CreationTime);
+            Console.WriteLine();
+        }
+    }
+    static class PAAFileManager
+    {
+        /// Записывает в txt файл содержимое 1-ого уровня указанного диска
+        public static void InspectDrive(string driveName)
+        {
+
+            DirectoryInfo dir = new DirectoryInfo(driveName);
+            FileInfo[] file = dir.GetFiles();
+            Directory.CreateDirectory(@"PAAInspect");
+            using (StreamWriter sw = new StreamWriter(@"PAAInspect\paadirinfo.txt"))
+            {
+                foreach (DirectoryInfo d in dir.GetDirectories())
+                    sw.WriteLine(d.Name);
+                foreach (FileInfo f in file)
+                    sw.WriteLine(f.Name);
+            }
+            File.Copy(@"PAAInspect\paadirinfo.txt", @"PAAInspect\paadirinfo_renamed.txt");
+            File.Delete(@"PAAInspect\paadirinfo.txt");
+        }
+        /// Создает папку PAAFiles, копирует в нее файлы из заданного пути по заданному расширению и перемещает папку в PAAFiles
+        public static void CopyFiles(string path, string ext)
+        {
+            string dirpath = @"PAAFiles";
+            Directory.CreateDirectory(dirpath);
+            DirectoryInfo di = new DirectoryInfo(path);
+            FileInfo[] files = di.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                if (file.Extension == ext)
+                    file.CopyTo($@"{dirpath}\{file.Name}");
+            }
+            Directory.Move(@"PAAFiles", @"PAAInspect\PAAFiles");
+            //DirectoryInfo dInfo = new DirectoryInfo("E:\\SAAFiles");
+            //dInfo.Create();
+
+            //string[] files2 = Directory.GetFiles("E:\\");
+
+            //foreach (string s in files2)
+            //{
+
+            //    File.Copy(s, "E:\\SAAFiles\\" + new FileInfo(s).Name);
+
+            //}
+            //foreach (FileInfo Info in dInfo.GetFiles())
+            //{
+            //    Info.CopyTo(path + "\\" + Info.Name, true);
+            //}
+        }
+        /// Архивация и разархивация
+        public static void ArchiveUnarchive()
+        {
+            string dirpath = @"PAAInspect\PAAFiles";
+            string zippath = @"PAAInspect\PAAFiles.zip";
+            string unzippath = @"Unzipped";
+            ZipFile.CreateFromDirectory(dirpath, zippath);
+            ZipFile.ExtractToDirectory(zippath, unzippath);
+        }
+
+    }
+    static class PAALog
+    {
+        static FileSystemWatcher watcher = new FileSystemWatcher
+        {
+            Path = @"E:\лабы\ООП\Lab13\Lab13\bin\Debug",
+            IncludeSubdirectories = true,
+            NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName
+        };
+        public static void Start()
+        {
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        public static void SearchByDate(string date)
+        {
+            watcher.EnableRaisingEvents = false;
+            using (StreamReader sr = new StreamReader("paalogfile.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    if (sr.ReadLine().StartsWith(date))
+                        Console.WriteLine(sr.ReadLine());
+                }
+            }
+        }
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("paalogfile.txt", true))
+            {
+                sw.WriteLine(DateTime.Now + "   " + e.ChangeType + "    путь: " + e.FullPath);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Начало слежения за Debug в папке с программой
+            Thread thread = new Thread(PAALog.Start);
+            thread.Start();
+
+            // Демонстрация работы LIADiskInfo
+            PAADiskInfo.FreeSpace("C:\\");
+            PAADiskInfo.FileSystemInfo("C:\\");
+            PAADiskInfo.DrivesFullInfo();
+
+            // Демонстрация работы PAADirInfo
+            PAADirInfo.FileCount(@"E:\лабы\ООП");
+            PAADirInfo.CreationTime(@"E:\лабы\ООП");
+            PAADirInfo.SubdirectoriesCount(@"E:\лабы\ООП");
+            PAADirInfo.ParentDirectory(@"E:\лабы\ООП");
+
+            // Демонстрация работы PAAFileInfo
+            PAAFileInfo.FullPath(@"D:\in.txt");
+            PAAFileInfo.BasicFileInfo(@"D:\in.txt");
+            PAAFileInfo.CreationTime(@"D:\in.txt");
+
+            // Демонстрация работы PAAFileManager
+            PAAFileManager.InspectDrive(@"E:\");
+            PAAFileManager.CopyFiles(@"PAAInspect", ".txt");
+            PAAFileManager.ArchiveUnarchive();
+
+            // Остановка процесса наблюдения
+            thread.Abort();
+
+            // Удаление каталогов
+            Console.WriteLine("Удалить каталоги? 1 - да");
+            int key = int.Parse(Console.ReadLine());
+            if (key == 1)
+            {
+                System.IO.Directory.Delete("PAAInspect", true);
+                System.IO.Directory.Delete("Unzipped", true);
+            }
+            // Демонстрация работы PAALog
+            //PAALog.SearchByDate("12.12.2018");
+        }
+    }
+}
